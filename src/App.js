daf919e88f3ad271a17777ff14e4329165f5afc8
zheng chai <zheng.chai@citrix.com>
2019-10-22 14:47:13 +0800

    React Hello world!
    Signed-off-by: zheng chai <zheng.chai@citrix.com>

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+function formatName(user) {
+	return user.firstName + ' ' + user.lastName;
+};
+
+const user = {
+	firstName: 'zheng',
+	lastName: 'chai'
+};
+
+function btnClicked() {
+	alert("Clicked");
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+           Hello, React your name is {formatName(user)}!
+      </div>
+      <button onClick={btnClicked} className="btn"> 
+	Click Me 
+      </button>
     </div>
   );
 }
